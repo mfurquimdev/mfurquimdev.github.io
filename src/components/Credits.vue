1f198105671fcfacbbafc9770c4472c457de9816
Mateus Furquim <furquim.axs@gmail.com>
2023-01-17 21:10:41 -0300

    Add underline on hover credit's link

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -7,19 +7,18 @@ const { t } = useI18n()
 
 <template>
   <p text-xs >
-    <em opacity-75>{{ t('intro.template') }}</em>
-      <a class="underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
+    <em opacity-75>{{ t('intro.template') }}&nbsp;</em>
+    <a class="underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">Vitesse</a>
   </p>
 </template>
 
 <style scoped>
-.nord-termynal {
-  --vt-color-bg: #2E3440;
-  --vt-color-text: #ECEFF4;
-  --vt-color-text-subtle: #66738e;
-  --vt-color-btn: #5E81AC;
-  --vt-color-btn-hover: #81A1C1;
+a {
+  color: #5E81AC;
+  text-decoration:none;
+
+}
+a:hover {
+   text-decoration:underline;
 }
 </style>
