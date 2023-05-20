33115e3a1fe8b5c46fe6640e61407b27f0e68d99
Mateus Furquim <furquim.axs@gmail.com>
2023-05-20 20:48:23 -0300

    Add experience section and rewrite about me

--- src/components/JobExperience.vue
+++ src/components/JobExperience.vue
@@ -9,5 +9,24 @@ const { t } = useI18n()
 <template>
 <div class="text-center">
     <h2>{{ t('experience.title') }}</h2>
+
+    <div class="text-justify">
+        <p>
+        {{ t('experience.p1') }}
+        </p>
+
+        <p>
+        {{ t('experience.p2') }}
+        </p>
+
+        <p>
+        {{ t('experience.p3') }}
+        </p>
+
+        <p>
+        {{ t('experience.p4') }}
+        </p>
+    </div>
+
 </div>
 </template>
